a73a841f5318ffff338c6a65c883051ac1347f0f
Andrey <corhal@yandex.ru>
2017-05-31 14:00:15 +0300

    feature generation

--- classes.py
+++ classes.py
@@ -34,29 +34,61 @@ class Developer(object):
             multiplier *= -1
         self.current_task.change_progress += self.productivity * multiplier
 
+class Requirement(object):
+    def __init__(self, tasks):
+        self.tasks = tasks
+        self.fulfilled = False
+
+    def add(self, tasks):
+        for task in tasks:
+            if task not in self.tasks:
+                self.tasks.append(task)
+
+    def check(self):
+        for task in self.tasks:
+            if task.status != "done":
+                return False
+        self.fulfilled = True
+        return self.fulfilled
+
 class Task(object):
-    def __init__(self, name, priority, max_progress):
+    def __init__(self, name, priority, max_progress, requirement=None):
         self.name = name
         self.status = "todo"
         self.progress = 0
         self.priority = priority
         self.max_progress = max_progress
+        self.requirement = requirement
+
+    @property
+    def is_blocked(self):
+        return not self.requirement.check()
 
     def change_progress(self, amount):
         self.progress += amount
         if self.progress >= self.max_progress:
             self.status = "done"
 
+    def __str__(self):
+        mystr = self.name + ": " + self.status + " " + str(self.progress) + "/" + str(self.max_progress)
+        mystr += ", blocked: " + str(self.is_blocked)
+        if self.requirement != None and len(self.requirement.tasks) > 0:
+            mystr += ", blocked by: ["
+            for task in self.requirement.tasks:
+                mystr += task.name
+            mystr += "]"
+        return mystr
+
 class Feature(Task):
-    def __init__(self, name, priority, max_progress):
+    def __init__(self, name, priority, max_progress, task_max_progress, max_length):
         Task.__init__(self, name, priority, max_progress)
         self.tasks = []
-        self.generate_tasks()
+        self.generate_tasks(task_max_progress, max_length)
 
-    def generate_tasks(self):
+    def generate_tasks(self, task_max_progress, max_length):
         task_max_progresses = []
         progress_left = self.max_progress
-        task_max_progress = random.randint(progress_left // 8, progress_left // 5)
+        # task_max_progress = random.randint(progress_left // 8, progress_left // 5)
         task_max_progresses.append(task_max_progress)
         progress_left -= task_max_progress
         while progress_left > 0:
@@ -67,7 +99,7 @@ class Feature(Task):
         lines = []
         starting_positions = []
         tasks_count = len(task_max_progresses)
-        max_length = random.randint(tasks_count // 3, tasks_count // 2)
+        # max_length = random.randint(tasks_count // 3, tasks_count // 2)
         tasks_count -= max_length
         line = [0] * max_length
         lines.append(line)
@@ -84,16 +116,55 @@ class Feature(Task):
             except ValueError:
                 starting_position = 0
             starting_positions.append(starting_position)
-
+        count = 1
         for line in lines:
             for i in range(len(line)):
                 task_progress = random.choice(task_max_progresses)
                 task_max_progresses.remove(task_progress)
-                line[i] = task_progress
+                requirement = Requirement([])
+                if i > 0:
+                    requirement.add([line[i-1]])
+                line[i] = Task("Task " + str(count), 1, task_progress, requirement)
+                print(line[i])
+                count += 1
+        real_positions = []
+        for i in range(len(lines)):
+            real_positions.append([])
+            start_pos = starting_positions[i]
+            for j in range(len(lines[i])):
+                real_positions[i].append(start_pos + j)
 
-        print("Max progress:" + str(self.max_progress))
+        print("Max progress: " + str(self.max_progress))
         print(lines)
         print(starting_positions)
+        print(real_positions)
 
+        for i in range(len(lines)):
+            for j in range(len(lines[i])):
+                if real_positions[i][j] > 0:
+                    additional_tasks_by_chances = {0.10: 2, 0.35: 1, 1.0: 0}
+                    if j == 0:
+                        additional_tasks_by_chances = {1.0: 1}
+                    dice_roll = random.random()
+                    for chance in sorted(additional_tasks_by_chances.keys()):
+                        if dice_roll <= chance:
+                            additional_tasks = additional_tasks_by_chances[chance]
+                    for d in range(additional_tasks):
+                        line_indices = []
+                        for k in range(len(starting_positions)):
+                            if starting_positions[k] < real_positions[i][j]:
+                                line_indices.append(k)
+                        random_index = random.choice(line_indices)
+                        random_line = lines[random_index]
+                        valid_tasks = []
+                        for k in range(len(real_positions[random_index])):
+                            if real_positions[random_index][k] < real_positions[i][j]:
+                                valid_tasks.append(random_line[k])
+                        blocker = random.choice(valid_tasks)
+                        lines[i][j].requirement.add([blocker])
 
-ft = Feature("f1", 1, 63)
+        for line in lines:
+            for task in line:
+                print(task)
+
+ft = Feature("f1", 1, 63, 15, 5)
